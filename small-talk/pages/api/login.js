0893c106e4e51c040ab2a74d5ec0f82aeff00eb5
Johnson Nguyen <nguyej20@unlv.nevada.edu>
2024-03-09 23:53:13 -0800

    added pages folder

---
+++ small-talk/pages/api/login.js
@@ -0,0 +1,33 @@
+import Mongoboi from "@/db/mongo"
+import { createCookieHash } from "@/util/smolCwypto.js"
+// POST, preferably with SSL because parameters with GET get cached all over the place
+export default async function POST(req, res) {
+  let data = {
+  }
+
+  if (req.body) {
+    let { username, password } = JSON.parse(req.body)
+
+    const query = {
+      username: username,
+      password: password
+    }
+    
+    const mongoboi = new Mongoboi(uri, "Users")
+    await mongboi.connect()
+    const result = await mongoboi.findOne("patients", query)
+    await mongoboi.disconnect()
+    if (result == null) { res.status(401).json({message: "FUCK"}); return; }
+    // create sessionID hash to store in mongoDB and user browser storage
+    const hash = createCookieHash({
+      username: username,
+      password: password,
+      timestamp: Date.now()
+    }, "FUCK") 
+    data = {
+      hash: hash
+    }
+  }
+
+  return res.status(200).json(data)
+}
