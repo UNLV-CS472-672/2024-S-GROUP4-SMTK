a808e7ac6bda922f7dc44347fe9c90399815e612
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2024-03-26 21:18:23 -0700

    Added test id

--- small-talk/app/login/page.js
+++ small-talk/app/login/page.js
@@ -46,7 +46,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center text-black">
+    <div data-testid='login' className="h-screen w-screen flex flex-col items-center justify-center text-black">
         <img src="/img/logo.png" alt="Description of the image"  width='300'></img>
       <div className="rounded-md bg-sky-500/50 p-5 m-4">
       {/* <form action={async (formData) => {
