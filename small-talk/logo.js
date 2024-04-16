530350bb4a0b9bb324ac4ce98da425f136cc9061
39otsu <mccodi11@gmail.com>
2024-04-15 20:07:15 -0700

    Created a layout component that will wrap around components. This includes a header and footer components that will comply with our responsiveness guidelines

---
+++ small-talk/logo.js
@@ -0,0 +1,10 @@
+export const Logo = () => {
+    return (
+        <div className="sm:flex items-center justify-center">
+            SMALL TALK
+        </div>
+        
+    )
+}
+
+export default Logo;
