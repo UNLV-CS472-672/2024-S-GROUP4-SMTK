cf5a1d452b896ea79d79a220b5f1de26baa60aee
39otsu <mccodi11@gmail.com>
2024-04-15 20:18:35 -0700

    wrong directory oops

--- small-talk/footer.js
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <div className="flex flex-col items-center w-screen h-[24px] text-slate-50 bg-slate-700 absolute bottom-0">
-            SmallTalk © {new Date().getFullYear()}
-        </div>
-    )
-}
-
-export default Footer;
