e8a2ae78db2c9441bc5cb125adbc74efc09b545a
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2024-04-16 19:40:11 -0700

    Merged main into the current branch. Added the chat vulgarity censor to the chatUtils sendMessage function

--- small-talk/util/chatUtils.js
+++ small-talk/util/chatUtils.js
@@ -1,12 +1,15 @@
 import handleMessageSave from "@/db/messageSave"
+import censor from "@/components/censorMess";
 
 export const sendMessage = (inputMessage, selectedUser, setPrivateMessages, setInputMessage, room, socket) => {
     if (typeof inputMessage === 'string' && inputMessage.trim() !== "" && selectedUser && typeof setPrivateMessages === 'function' && typeof setInputMessage === 'function') {
-        console.log("sent message from:" + selectedUser.username + " with message: " + inputMessage);
-        socket.emit("private message", { content: inputMessage, to: selectedUser.userID});
-        setPrivateMessages(prevMessages => [...prevMessages, { content: inputMessage, from: socket.id }]);
+        const censorMessage = censor(inputMessage);
+        
+        console.log("sent message from:" + selectedUser.username + " with message: " + censorMessage);
+        socket.emit("private message", { content: censorMessage, to: selectedUser.userID});
+        setPrivateMessages(prevMessages => [...prevMessages, { content: censorMessage, from: socket.id }]);
         setInputMessage("");
-        handleMessageSave(inputMessage);
+        handleMessageSave(censorMessage);
     } else {
         console.log("No user selected or empty message");
     }
