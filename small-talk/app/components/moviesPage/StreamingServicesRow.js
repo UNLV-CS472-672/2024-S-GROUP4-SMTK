ae67658c4f04eb499fa222b19b5348d9525df532
Alvaro Hernandez <156855195+AlvaroH2001@users.noreply.github.com>
2024-04-22 19:07:31 -0700

    Added movies and streaming services components (#187)
    Co-authored-by: Alvaro Hernandez <alvarohernandez@alvaros-air.mynetworksettings.com>

---
+++ small-talk/app/components/moviesPage/StreamingServicesRow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import StreamingServiceCard from './StreamingServiceCard';
+import servicesData from '../../data/moviesData/streamingServices.json'; // Path to your streaming services JSON data
+
+function StreamingServicesRow({ title }) {
+    return (
+        <div className="mb-8">
+            <h2 className="text-white text-xl mb-4">{title}</h2>
+            <div className="flex overflow-x-auto space-x-4 pl-4">
+                {servicesData.map(service => (
+                    <StreamingServiceCard key={service.id} service={service} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default StreamingServicesRow;
