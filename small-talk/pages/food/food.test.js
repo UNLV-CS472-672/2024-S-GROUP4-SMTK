cb8d5e2b655b7dc3577f28180b51233574c5d0cf
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2024-04-22 20:13:35 -0700

    Added food page to layout

--- small-talk/pages/food/food.test.js
+++ small-talk/pages/food/food.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Food from './page'; // Ensure the correct file path and component name are used
-import ThemeLayout from '../components/ThemeLayout'; // Ensure correct path
-import MenuTabs from '../components/foodPage/MenuTabs'; // Ensure correct path
+import ThemeLayout from '../../app/components/ThemeLayout'; // Ensure correct path
+import MenuTabs from '../../app/components/foodPage/MenuTabs'; // Ensure correct path
 
 jest.mock('../components/foodPage/MenuTabs', () => {
     const MenuTabsMock = () => <div>MenuTabs Mock</div>;
