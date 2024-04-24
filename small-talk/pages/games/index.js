f9caf89f21fb424e9b6a3f77c74a8859d55801ee
Alvaro Hernandez <alvarohernandez@alvaros-air.mynetworksettings.com>
2024-04-23 22:10:58 -0700

    Updated all of the page headers and added homepage content

--- small-talk/pages/games/index.js
+++ small-talk/pages/games/index.js
@@ -4,6 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from "@/components/layout";
 import GameTabs from '../../app/components/gamesPage/gameTabs';
+import Title from '@/components/title';
 
 // ai-gen start (ChatGPT-3.5, 2)
 // used ai for documentation and match the format as other pages
@@ -17,9 +18,8 @@ export const Game = () => {
     return (
         <div className='bg-slate-800 flex flex-col min-h-screen'>
             <Layout width={windowWidth} renderHeader={true} renderFooter={true}>
+            <Title page="Games"/>
                 <div className="flex-grow p-4">  {/* Added padding for all sides */}
-                    <h1 className="text-center mb-10 text-white">Game Page</h1>
-
                     <GameTabs />
 
                 </div>
