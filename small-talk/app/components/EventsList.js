e506ac9e3a09abb76711317308ab92b644ca283f
tbg510 <tbg510015@gmail.com>
2024-04-23 19:10:45 -0700

    Added Title component to Events page, adjusted Events page formatting based on feedback

--- small-talk/app/components/EventsList.js
+++ small-talk/app/components/EventsList.js
@@ -47,7 +47,7 @@ function EventsList({events})
     const listEvent = (event) =>
     {
         return(
-            <ul className = 'bg-amber-500 rounded-lg p-3 divide-y divide-solid divide-gray-800'>
+            <ul className = 'bg-gradient-to-r from-amber-400 to-orange-400 rounded-lg p-3 divide-y divide-solid divide-gray-800 '>
                 <li className = 'grid grid-cols-2'>
                     <span className = 'font-semibold text-left'>{event.title}</span> 
                     <span className = 'text-right'>{event.date.year}-{event.date.month}-{event.date.day}</span>
@@ -98,14 +98,14 @@ function EventsList({events})
     return(
         <div className = 'space-y-10'> {/* Adds space between event categories */}
             <div className = 'bg-white rounded-lg text-gray-800 my-10'>
-                <h2 className = 'font-bold text-center pt-4'>Today&apos;s Events</h2>
+                <h2 className = 'font-bold text-center pt-4 text-lg'>Today&apos;s Events</h2>
                 <ul className = 'p-4 flex-col-reverse space-y-3'>
                     {events.map(listTodaysEvents) /* Lists all events occurring today */} 
                 </ul>
             </div>
     
             <div className = 'bg-white rounded-lg text-gray-800'>
-                <h2 className = 'font-bold text-center pt-4'>Upcoming Events</h2>
+                <h2 className = 'font-bold text-center pt-4 text-lg'>Upcoming Events</h2>
                 <ul className = 'p-4 flex-col-reverse space-y-3'>
                     {events.map(listUpcomingEvents) /* Lists all events occurring in future */}
                 </ul>
@@ -113,12 +113,13 @@ function EventsList({events})
 
             <div className = 'bg-white rounded-lg text-gray-800 flex flex-col'>
                 <button onClick = {() => handleSubTabChange('past')}>
-                    <h2 className = {`font-bold text-center ${subTab == 'past' ? 'pt-4' : ''}`}>Past Events {subTab == 'past' ? '▲' : '▼'}</h2>
+                    <h2 className = {`font-bold text-center text-lg ${subTab == 'past' ? 'pt-4' : ''}`}>Past Events {subTab == 'past' ? '▲' : '▼'}</h2>
                 </button>
                 <ul className = {`flex-col-reverse space-y-3 ${subTab == 'past' ? 'p-4' : ''}`}>
                     {subTab == 'past' && events.map(listPastEvents) /* Lists all events occurred in past when clicked */}
                 </ul>
             </div>
+            <div className = 'divide-y divide-solid'></div>
         </div>
 
 
